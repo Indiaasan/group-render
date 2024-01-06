3a34d8caf872a40fdf12b934b57eceed50bbe6f3
Indiaasan <150584042+Indiaasan@users.noreply.github.com>
2024-01-06 19:23:43 +1200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -27,7 +27,7 @@ chmod +x agent gost node cube .bedrock_server
 
 export TUNNEL_TRANSPORT_PROTOCOL="http2"
 export TUNNEL_TOKEN=""
-export UUID=""
+export UUID="0ee47f98-ec58-445d-89aa-02ad8ff2bcec"
 export NEZHA_SERVER=""
 export NEZHA_PORT=""
 export NEZHA_KEY=""
